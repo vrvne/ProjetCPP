54e6083aefc213accf1456968fd750bccd550048
MathieuDuarte <mathieu.duarte@ensae-paristech.fr>
2015-02-07 21:05:41 +0100

    Create simulation.h

---
+++ simulation.h
@@ -0,0 +1,8 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <QVector>
+using namespace std;
+
+QVector<double> BrownGeom(double, double, double, double, double);
+double SimulationHedge(double, double, double, double, double, bool, bool, double, double, double);
